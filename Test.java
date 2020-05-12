dd718b0efded7bdcb54c83f7114c973d5b1e438d
HeoITNuce <lethuylinhleloi@gmail.com>
2020-05-12 21:27:56 +0700

    add Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+package test;
+
+import ui.MainUI;
+
+public class Test {
+
+	public static void main(String[] args) {
+		MainUI ui=new MainUI("Quản lý lớp học");
+		ui.showWindow();
+	}
+
+}
+qqqqqq
